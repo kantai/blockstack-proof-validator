f148bbd0b788306aa10bcbf9041166d97b6f3211
Aaron Blankstein <aaron@blockstack.com>
2017-11-04 20:38:43 -0400

    cleanup

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser');
 
+// $ npm i
+// $ node index.js
+
 function server () {
   var app = express()
   app.use(cors())
   app.use(bodyParser.json())
 
   app.post('/validate', function(req, res, next) {
-    console.log(req)
-
     profile = req.body.profile
     address = req.body.address
 
@@ -24,6 +25,3 @@ function server () {
 let app = server()
 
 app.listen(80, 'localhost')
-
-//fetch('https://gaia.blockstack.org/hub/12mGibk1Y6xZXwcJruG2xZTbjuZ1tWvLqo/0/profile.json').then( resp => resp.json() ).then( x => x[0].decodedToken.payload.claim ).then( p => fetch( 'http://localhost/validate', { method : 'POST', body : JSON.stringify({profile : p, address : '12mGibk1Y6xZXwcJruG2xZTbjuZ1tWvLqo'} ) } )).then(console.log)
-//
